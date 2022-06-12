00054b486c026064cce4ca84948c5b375262d4ab
Jhironsel Diaz <jhironseld@gmail.com>
2022-06-12 02:19:50 -0400

    Cambiando Cosas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{provide: APP_BASE_HREF, useValue: '/my/app'}]
 })
 export class AppComponent {
   title = 'curriculum';
